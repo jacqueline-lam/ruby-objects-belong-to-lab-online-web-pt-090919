0c80d9af98fb8b29dcd4b94ff36855a71de0f458
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-10-10 00:27:32 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,7 +1,7 @@
 class Post
   # has a title  
   # Post belongs to an author (post.author  = author)
-  # knows     name of author it belongs to
+  # knows name of author it belongs to
   attr_accessor :title, :author
   
 end
